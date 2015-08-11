ff02ad6254634a11b4d4440b51a5341f47556558
luismiglietti <luismiglietti@gmail.com>
2015-08-11 18:17:08 -0430

    basic card functionality

--- dist/sgb-screen-flash-cards.js
+++ dist/sgb-screen-flash-cards.js
@@ -2,15 +2,42 @@
 'use strict';
 
 angular.module('sgb-screen-flash-cards', ['megazord'])
-    .controller('sgb-screen-detail-flash-cards', ['$stateParams', '_screen', '_screenParams', '$scope',
-    		   function ($stateParams, _screen, _screenParams, $scope) {
+    .controller('sgb-screen-flash-cards-controller', ['$stateParams', '_screen', '_screenParams', '$scope','$timeout',
+    		   function ($stateParams, _screen, _screenParams, $scope, $timeout) {
 
         _screen.initialize($scope, _screenParams);
-        $scope.data = $stateParams.data;
-        $scope.templateType = _screenParams.templateType; 
+        $scope.cards = $stateParams.data;
+        var selectedCard = null;
+        var WAIT = false;
+        $scope.rotate = false; 
+        $scope.index = 0; 
+        $scope.content = {
+        	titleToShow : $scope.cards[0].title,
+        	textToShow : $scope.cards[0].text_1
+        }
+
+        $scope.flip = function(index) {
+      		$scope.rotate = !$scope.rotate;
+      		$scope.content.textToShow = $scope.rotate ? $scope.cards[index].text_2 : $scope.cards[index].text_1; 
+      		//var myEl = angular.element(document.querySelector('.no-rotate'));
+			//myEl.removeClass('.flipped');
+      		//flip(card, 20); 
+    	}
+
+    	function flip(card, i, callback) {
+      		$timeout(function() {
+        		card.isFlipped = false;
+        		WAIT = false;
+        		if (callback) callback();
+      		}, 10 * (i || 30) );
+    	}
+
+
+        //$scope.templateType = _screenParams.templateType; 
 
         //Delete after mz framework gets updated
         //$scope.screenType = "sgb-screen-detail";
 
     }]); 
+
 },{}]},{},[1]);
