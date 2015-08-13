8cc31e1371afe3fddcc3ef3cb66e4c3768f0076c
luismiglietti <luismiglietti@gmail.com>
2015-08-13 11:10:50 -0430

    completely changed screen animation, css and template

--- src/sgb-screen-flash-cards.js
+++ src/sgb-screen-flash-cards.js
@@ -6,44 +6,50 @@ angular.module('sgb-screen-flash-cards', ['megazord'])
 
         _screen.initialize($scope, _screenParams);
         $scope.cards = $stateParams.data;
-        $scope.flipped = false; 
-        $scope.backCard = false; 
-        $scope.changeCard = false; 
         $scope.index = 0; 
         $scope.currentCard = angular.copy($scope.cards[$scope.index]);
+        $scope.flipped = false;
+        
+        $scope.flip = function() {
+            $scope.flipped = !$scope.flipped;
+        };
 
-    	$scope.waitAnim = function() {
-    		$timeout(function() {		
-    			$scope.changeCard = false;  
-			}, 270);
-    	}
+        $scope.nextCard = function () {
+            $scope.index =  $scope.index == $scope.cards.length-1? 0 : $scope.index +1;
+            $scope.currentCard = angular.copy($scope.cards[$scope.index]);
+        }
 
-       $scope.flip = function() {
-        	$scope.flipped = true
-        	$timeout(function() {
-        	 	$scope.flipped=false;
-        	 	$scope.backCard = !$scope.backCard;
-			 }, 270);
-    	}
+        $scope.previousCard = function () {
+            $scope.index = $scope.index==0 ? $scope.cards.length-1 : $scope.index-1;
+            $scope.currentCard = angular.copy($scope.cards[$scope.index]);
+           
+        }   
 
-    	$scope.animate = function() {
-    		$scope.currentCard.hide = true; 
-    	    $scope.flipped = false; 
-    	    $scope.changeCard = true; 
-    	}
+    }])
 
-    	$scope.previousCard = function () {
- 			$scope.animate(); 
-			$scope.index = $scope.index==0 ? $scope.cards.length-1 : $scope.index-1;
-    	    $scope.currentCard = angular.copy($scope.cards[$scope.index]);
-    		$scope.waitAnim(); 
-    	}	
+    .directive("flipper", function() {
+        return {
+            restrict: "E",
+            template: "<div class='flipper' ng-transclude ng-class='{ flipped: flipped }'></div>",
+            transclude: true,
+            scope: {
+                flipped: "="
+            }
+        };
+    }).
 
-    	$scope.nextCard = function () {
-    		$scope.animate(); 
-    		$scope.index =  $scope.index == $scope.cards.length-1? 0 : $scope.index +1;
-			$scope.currentCard = angular.copy($scope.cards[$scope.index]);
-			$scope.waitAnim(); 
-    	}
+    directive("front", function() {
+        return {
+            restrict: "E",
+            template: "<div class='front tile' ng-transclude></div>",
+            transclude: true
+        };
+    })
 
-    }]);
+    .directive("back", function() {
+        return {
+            restrict: "E",
+            template: "<div class='back tile' ng-transclude></div>",
+            transclude: true
+        }
+    });
